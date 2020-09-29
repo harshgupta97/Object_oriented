4cb2022a5077949901914226213da9a19749eb47
Harsh Gupta <warmachine476@gmail.com>
2020-09-29 16:18:21 +0530

    override equal and hashcode method in point and compare objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        var control = new UIControl(true);
-        var textbox = new TextBox();
-        show(control );
+//        var control = new UIControl(true);
+//        var textbox = new TextBox();
+//        show(control );
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
+        System.out.println(point1.equals(point1));
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
+
     }
 
-    public static void show(UIControl control) {
-        var textbox= (TextBox)control;
-        textbox.setText("Hello World");
-        System.out.println(control);
-    }
+//    public static void show(UIControl control) {
+//        var textbox= (TextBox)control;
+//        textbox.setText("Hello World");
+//        System.out.println(control);
+//    }
 }
